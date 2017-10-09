c5d8477d0e94a36161680810bdbe164a229b642e
ODET Alexandre <madddogg39@gmail.com>
2017-10-09 14:08:07 +0200

    Now all temperatures Label text update with segmentedControl Index

--- Weather/Controllers/Detail/DetailWeatherViewController.swift
+++ Weather/Controllers/Detail/DetailWeatherViewController.swift
@@ -24,12 +24,15 @@ class DetailWeatherViewController: UIViewController {
   @IBOutlet weak var weatherSegmentedControl: UISegmentedControl!
   @IBOutlet weak var sunriseLabel: UILabel!
   @IBOutlet weak var sunsetLabel: UILabel!
-
+  @IBOutlet weak var minimalTemperatureLabel: UILabel!
+  @IBOutlet weak var maximalTemperatureLabel: UILabel!
+  
   private var temperatureIndex: Observable<Int> {
     return weatherSegmentedControl
       .rx
       .selectedSegmentIndex
       .asObservable()
+      .share()
   }
   
   override func viewDidLoad() {
@@ -54,6 +57,28 @@ class DetailWeatherViewController: UIViewController {
       }
       .bind(to: cityTemperatureLabel.rx.text)
       .disposed(by: disposeBag)
+    
+    temperatureIndex.map { [unowned self] index -> String in
+      let temperatureExtension = Temperature(intValue: index).printableMetrics
+      let currentTemp = self.viewModel.currentWeather.weatherInfos.minimalTemperature!
+      let convertedTemperature = (index == 0) ? Converter.convertKelvinToCelsius(kelvin: currentTemp) : Converter.convertKelvinToFahrenheit(kelvin: currentTemp)
+      let nf = NumberFormatter()
+      nf.numberStyle = .decimal
+      nf.maximumFractionDigits = 1
+      let temperatureString = nf.string(from: NSNumber(value: convertedTemperature)) ?? "--"
+      return "\(String(describing: temperatureString))\(temperatureExtension)"
+      }.bind(to: minimalTemperatureLabel.rx.text).disposed(by: disposeBag)
+    
+    temperatureIndex.map { [unowned self] index -> String in
+      let temperatureExtension = Temperature(intValue: index).printableMetrics
+      let currentTemp = self.viewModel.currentWeather.weatherInfos.maximalTemperature!
+      let convertedTemperature = (index == 0) ? Converter.convertKelvinToCelsius(kelvin: currentTemp) : Converter.convertKelvinToFahrenheit(kelvin: currentTemp)
+      let nf = NumberFormatter()
+      nf.numberStyle = .decimal
+      nf.maximumFractionDigits = 1
+      let temperatureString = nf.string(from: NSNumber(value: convertedTemperature)) ?? "--"
+      return "\(String(describing: temperatureString))\(temperatureExtension)"
+      }.bind(to: maximalTemperatureLabel.rx.text).disposed(by: disposeBag)
     
     viewModel.sunriseDateTimestamp.map { [unowned self] timestamp -> String in
       let location = CLLocation(latitude: self.viewModel.currentWeather.coordinates.latitude!,
